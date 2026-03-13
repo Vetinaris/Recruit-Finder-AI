14a15762755bb230b632437d85e6b03ae04a1b84
Vetinaris <148788817+Vetinaris@users.noreply.github.com>
2026-03-13 15:29:16 +0100

    Initial commit - ASP.NET structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Recruit_Finder_AI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
